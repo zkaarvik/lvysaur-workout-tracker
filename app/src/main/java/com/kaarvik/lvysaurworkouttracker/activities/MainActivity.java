d942eccfef721d19189ff85d1b94ab403cb3696b
Zach <zkaarvik@gmail.com>
2017-02-08 19:31:21 -0800

    Switch to proper nav drawer

--- app/src/main/java/com/kaarvik/lvysaurworkouttracker/activities/MainActivity.java
+++ app/src/main/java/com/kaarvik/lvysaurworkouttracker/activities/MainActivity.java
@@ -2,22 +2,18 @@ package com.kaarvik.lvysaurworkouttracker.activities;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.AdapterView;
 
 import com.kaarvik.lvysaurworkouttracker.R;
-import com.kaarvik.lvysaurworkouttracker.adapters.DrawerListAdapter;
-import com.kaarvik.lvysaurworkouttracker.data.DrawerListItem;
 import com.kaarvik.lvysaurworkouttracker.fragments.FeedbackFragment;
 import com.kaarvik.lvysaurworkouttracker.fragments.GraphsFragment;
 import com.kaarvik.lvysaurworkouttracker.fragments.SettingsFragment;
@@ -25,13 +21,12 @@ import com.kaarvik.lvysaurworkouttracker.fragments.WorkoutHistoryFragment;
 
 import io.realm.Realm;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private Realm realm;
 
-    private DrawerListItem[] drawerListItems;
     private DrawerLayout drawerLayout;
-    private ListView drawerList;
     private ActionBarDrawerToggle drawerToggle;
 
     private Toolbar toolbar;
@@ -46,10 +41,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //Set up navigation drawer
-        initializeDrawer();
+        initializeDrawer(toolbar);
 
         //Set default fragment
-        selectMenuItem(0);
+        selectMenuItem(R.id.nav_workout);
 
         //Get realm instance
         realm = Realm.getDefaultInstance();
@@ -61,68 +56,42 @@ public class MainActivity extends AppCompatActivity {
         realm.close();
     }
 
-    private void initializeDrawer() {
-        drawerListItems = DrawerListItem.DrawerListItemFactory(this);
-
+    private void initializeDrawer(Toolbar toolbar) {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerList = (ListView) findViewById(R.id.left_drawer);
 
-        drawerList.setAdapter(new DrawerListAdapter(this, drawerListItems));
-        drawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
-                R.string.drawer_open, R.string.drawer_close) {
-
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getSupportActionBar().setTitle(appTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle(appTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-
-        //Set drawerToggle as the drawerListener
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close);
         drawerLayout.addDrawerListener(drawerToggle);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-    }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectMenuItem(position);
-        }
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
         drawerToggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_workout);
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        selectMenuItem(id);
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -136,51 +105,29 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /* Called whenever we call invalidateOptionsMenu() */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
-//        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
     /** Swaps fragments in the main content view */
-    private void selectMenuItem(int position) {
+    private void selectMenuItem(int id) {
         // Create a new fragment according to selection
-        Fragment fragment = getFragmentForSelection(position);
+        Fragment fragment = getFragmentForSelection(id);
 
         // Insert the fragment by replacing any existing fragment
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.content_frame, fragment)
                 .commit();
-
-        // Highlight the selected item, update the title, and close the drawer
-        drawerList.setItemChecked(position, true);
-        setTitle(drawerListItems[position].getTitle());
-        drawerLayout.closeDrawer(drawerList);
     }
 
-    private Fragment getFragmentForSelection(int position) {
-        String selectedListItem = drawerListItems[position].getId();
-        Fragment fragment;
+    private Fragment getFragmentForSelection(int id) {
+        Fragment fragment = null;
 
-        switch (selectedListItem) {
-            default:
-            case DrawerListItem.FRAGMENT_WORKOUT:
-                fragment = new WorkoutHistoryFragment();
-                break;
-            case DrawerListItem.FRAGMENT_GRAPHS:
-                fragment = new GraphsFragment();
-                break;
-            case DrawerListItem.FRAGMENT_SETTINGS:
-                fragment = new SettingsFragment();
-                break;
-            case DrawerListItem.FRAGMENT_FEEDBACK:
-                fragment = new FeedbackFragment();
-                break;
-
+        if (id == R.id.nav_workout) {
+            fragment = new WorkoutHistoryFragment();
+        } else if (id == R.id.nav_graphs) {
+            fragment = new GraphsFragment();
+        } else if (id == R.id.nav_settings) {
+            fragment = new SettingsFragment();
+        } else if (id == R.id.nav_feedback) {
+            fragment = new FeedbackFragment();
         }
 
         return fragment;
